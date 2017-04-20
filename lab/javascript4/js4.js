712a67628b4d08d4d75a200fdd7e48ae3d81e316
holly <parkh012@newschool.edu>
2017-04-20 10:06:10 -0400

    javascript lab 4

---
+++ lab/javascript4/js4.js
@@ -0,0 +1,35 @@
+var colors = ['red',
+			  'green',
+			  'blue',
+			  ];
+
+var objects = ['img/bunny.jpg',
+			   'img/cat.jpg',
+			   'img/dog.jpg',
+			   'img/sloth.jpg',
+			   'img/circle.png',
+			   'img/square.jpg',
+			   'img/triangle.png',
+			   'Meow',
+			   '...',
+			   'Woof',
+			   'Meep',
+			   ];
+
+$('.button').click(function() {
+	var random_object_number = Math.floor(Math.random() * objects.length);
+	var object_to_use = objects[random_object_number];
+
+	var random_color_number = Math.floor(Math.random() * colors.length);
+	var color_to_use = colors[random_color_number];
+
+
+	var result = '';
+	if (random_object_number < 7) {
+		result = '<div class="object"><img src="' + object_to_use + '" /></div>';
+	} else {
+		result ='<div class="object ' + color_to_use + '">' + object_to_use + '</div>';
+	}
+
+	$('.content').prepend(result);
+});
